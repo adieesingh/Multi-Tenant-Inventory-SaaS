ae39238238884cba14ab15b101706cb52ee9817b
unknown <1606adarsh@gmail.com>
2026-07-25 19:50:12 +0530

    Added cors

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -15,7 +15,10 @@ import cookieParser from "cookie-parser"
 const app = express();
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}));
 
 app.use("/api/addOrganizations",organizationRouter)
 app.use("/api/user",userRouter)
