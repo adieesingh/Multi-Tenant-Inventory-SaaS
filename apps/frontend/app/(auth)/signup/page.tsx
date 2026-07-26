264dc4729fe2a833f3b57b29e680bd5abd033ba5
unknown <1606adarsh@gmail.com>
2026-07-26 12:47:41 +0530

    Fix

--- apps/frontend/app/(auth)/signup/page.tsx
+++ apps/frontend/app/(auth)/signup/page.tsx
@@ -1,105 +1,100 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Button } from "@base-ui/react/button";
-import { Input } from "@base-ui/react/input";
-import Link from "next/link";
+"use client";
+import { useForm } from "react-hook-form";
+import { SignupPage, SignupSchema } from "@repo/common/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AspectRatioPortrait } from "@/components/ui/aspect-ratio";
+import { FieldInput } from "@/components/ui/field";
+import { ButtonLink } from "@/components/ui/button";
+import axios from "axios";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-export default function Signup(){
-    
-    return ( <main className="flex min-h-screen items-center justify-center bg-gray-50 p-6">
-      <Card className="w-full max-w-md rounded-2xl border shadow-lg">
-        <CardContent className="p-8">
-          <div className="space-y-2 text-center">
-            <h1 className="text-4xl font-bold tracking-tight">
-               Create a workspace
-            </h1>
-
-            <p className="text-sm text-muted-foreground">
-              Welcome ! Please sign up.
-            </p>
-          </div>
-
-          <form className="mt-8 space-y-6">
-            <div className="space-y-2">
-              <Label htmlFor="Enter a organization Name">Organization Name</Label>
-
-              <Input
-                id="name"
-                type="text"
-                placeholder="Singh Footwear"
-                className={"w-full p-2 rounded-md border"}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="Enter a email">Email</Label>
-
-              <Input
-                id="email"
-                type="email"
-                placeholder="john@example.com"
-                className={"w-full p-2 rounded-md border"}
-              />
-            </div>
-             <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <Label htmlFor="address">Address</Label>
-
-               
-              </div>
-
-              <Input
-                id="address"
-                type="text"
-                placeholder="RN 12,BLG-1,MUMBAI,MAH"
-                className={"w-full p-2 rounded-md border"}
-              />
-            </div>
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <Label htmlFor="phone">Phone No</Label>
-
-               
-              </div>
-
-              <Input
-                id="phone"
-                type="tel"
-                placeholder="+919876543210"
-                className={"w-full p-2 rounded-md border"}
-              />
-            </div>
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <Label htmlFor="password">Password</Label>
-
-               
-              </div>
-
-              <Input
-                id="password"
-                type="password"
-                placeholder="••••••••"
-                className={"w-full p-2 rounded-md border"}
-              />
-            </div>
-
-            <Button className="w-full bg-blue-500  hover:bg-blue-900 hover:cursor-pointer p-2 rounded-lg text-white"  >
-              Sign Up
-            </Button>
-
-            <p className="text-center text-sm text-muted-foreground">
-              Already have an workspace?{" "}
-              <Link
-                href="/login"
-                className="font-medium text-primary hover:underline"
-              >
-                Sign in
-              </Link>
-            </p>
-          </form>
-        </CardContent>
-      </Card>
+export default function Signup() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignupPage>({
+    resolver: zodResolver(SignupSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+  });
+  const handleSignup = async (data: SignupPage) => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/user/`,
+        data,
+        {
+          withCredentials: true,
+        },
+      );
+      if (response.status == 201) {
+        toast.success(response.data.message);
+        router.push("/dashboard");
+      }
+      if (response.status == 401) {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data.message || "Something went wrong");
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
+  };
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50 p-6">
+      <div className=" w-full max-w-md bg-white shadow-lg rounded-2xl">
+        <form
+          onSubmit={handleSubmit(handleSignup)}
+          className="space-y-6 max-w-md w-full items-center flex flex-col"
+        >
+          <AspectRatioPortrait
+            alt="Logo"
+            ratio={16 / 9}
+            src="/logo.png"
+          ></AspectRatioPortrait>
+          <h2 className="text-gray-900 text-center text-4xl font-bold">
+            Signup
+          </h2>
+          <p className="text-gray-500 text-center text-sm">Please ! Sign Up</p>
+          <FieldInput
+            id="name"
+            disabled={isSubmitting}
+            label="Name"
+            placeholder="John"
+            type="text"
+            {...register("name")}
+          ></FieldInput>
+          {errors.name && <p>{errors.name.message}</p>}
+          <FieldInput
+            id="email"
+            label="Email"
+            placeholder="john@exapmle.com"
+            type="email"
+            {...register("email")}
+          ></FieldInput>
+          <FieldInput
+            id="password"
+            label="Password"
+            placeholder="•••••••"
+            type="password"
+            {...register("password")}
+          ></FieldInput>
+          <ButtonLink
+            disabled={isSubmitting}
+            text={isSubmitting ? "Signup .." : "Signup"}
+            type="submit"
+            variant={"primary"}
+            
+          ></ButtonLink>
+        </form>
+      </div>
     </main>
-    )
-  
+  );
 }
