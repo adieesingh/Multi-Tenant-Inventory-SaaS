a86f30a93cb2177c1c5256d0b74baec8c9c5b94a
unknown <1606adarsh@gmail.com>
2026-07-27 21:09:14 +0530

    Added

---
+++ apps/frontend/app/(dashboard)/categories/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Navbar } from "@/components/ui/navbar";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
+import { Sidebar } from "@/components/ui/sidebar";
+
+export default function DashboardLayout(){
+    return <div className="min-h-screen bg-white">
+       
+    </div>
+}
---
+++ apps/frontend/app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { AppSideBar } from "@/components/ui/app-sidebar";
+import {  SidebarProvider } from "@/components/ui/sidebar";
+import  { ReactNode } from "react";
+
+export default function DashboardLayout({children}:{children:ReactNode}){
+    return (
+        <SidebarProvider>
+      <AppSideBar/>
+      <main className=" p-6">{children}</main>
+    </SidebarProvider>
+
+    )
+}
