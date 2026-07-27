37b0cf30ccbccf56bbdbe71537d79a4b61039c62
unknown <1606adarsh@gmail.com>
2026-07-27 21:11:51 +0530

    Added button

--- apps/frontend/components/ui/field.tsx
+++ apps/frontend/components/ui/field.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import { Input } from "./Input";
+import { Input } from "./input";
 
 function FieldSet({ className, ...props }: React.ComponentProps<"fieldset">) {
   return (
