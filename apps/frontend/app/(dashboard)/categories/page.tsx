c8675eb0784908ee3f227a697e95de76e3fce9cf
unknown <1606adarsh@gmail.com>
2026-07-29 21:18:18 +0530

    Add sidebar

--- apps/frontend/app/(dashboard)/categories/page.tsx
+++ apps/frontend/app/(dashboard)/categories/page.tsx
@@ -1,10 +1,9 @@
 "use client"
-import { Navbar } from "@/components/ui/navbar";
-import { NavigationMenu } from "@/components/ui/navigation-menu";
-import { Sidebar } from "@/components/ui/sidebar";
+
+
 
 export default function DashboardLayout(){
     return <div className="min-h-screen bg-white">
-       
+        
     </div>
 }
