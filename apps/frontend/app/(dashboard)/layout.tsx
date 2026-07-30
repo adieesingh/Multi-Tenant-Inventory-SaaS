4d4fcf32d13b8973020a0d687ca5ef842c100b02
unknown <1606adarsh@gmail.com>
2026-07-30 20:10:55 +0530

    added navbar and sidebar

--- apps/frontend/app/(dashboard)/layout.tsx
+++ apps/frontend/app/(dashboard)/layout.tsx
@@ -1,14 +1,23 @@
-
 import { AppSideBar } from "@/components/ui/app-sidebar";
-import {  SidebarProvider } from "@/components/ui/sidebar";
-import  { ReactNode } from "react";
+import { Navbar } from "@/components/ui/navbar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ReactNode } from "react";
 
-export default function DashboardLayout({children}:{children:ReactNode}){
-    return (
-        <SidebarProvider>
-      <AppSideBar/>
-      <main className="p-6 ">{children}</main>
+export default function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      {/* 1. Sidebar hamesha Provider ke andar pehle aayega */}
+      <AppSideBar />
+
+      {/* 2. SidebarInset poore right-side (Navbar + Main Content) ko containerize karta hai */}
+      <SidebarInset>
+        <Navbar />
+        <main className="flex-1 p-6 bg-gray-50">{children}</main>
+      </SidebarInset>
     </SidebarProvider>
-
-    )
+  );
 }
