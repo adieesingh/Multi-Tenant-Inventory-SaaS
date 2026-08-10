ce6236281dbf56daa261f4fd5db36ad47f491dbc
unknown <1606adarsh@gmail.com>
2026-08-10 20:37:07 +0530

    Button

--- apps/frontend/components/ui/button.tsx
+++ apps/frontend/components/ui/button.tsx
@@ -2,7 +2,8 @@ import { Button as ButtonPrimitive } from "@base-ui/react/button";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ForwardRefExoticComponent, ReactNode, RefAttributes } from "react";
+import { LucideIcon, LucideProps } from "lucide-react";
 
 const buttonVariants = cva(
   "group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
@@ -61,11 +62,12 @@ interface ButtonProps extends VariantProps<typeof buttonVariants> {
     className:string
   disabled?: boolean;
   type?: "submit" | "button";
+  icon?:ReactNode
 }
-function ButtonLink({ text, type, disabled,className, ...props }: ButtonProps) {
+function ButtonLink({ text, type, disabled,className,icon, ...props }: ButtonProps) {
   return (
     <Button type={type} {...props} disabled={disabled} className={className} {...props}>
-      {text}
+    {icon}  {text}
     </Button>
   );
 }
