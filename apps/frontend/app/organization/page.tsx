bfe5b30ad39b5c7d7f9c47b45e668e715fc24890
unknown <1606adarsh@gmail.com>
2026-08-10 20:38:34 +0530

    Organizaion page

--- apps/frontend/app/organization/page.tsx
+++ apps/frontend/app/organization/page.tsx
@@ -1,5 +1,6 @@
 import { colunms } from "@/app/column";
 import { DataTable } from "@/components/ui/data-table";
+import { ToolBar } from "@/components/ui/toolbar";
 
 import { getOrganization } from "@/service/organization";
 
@@ -7,8 +8,10 @@ import { getOrganization } from "@/service/organization";
 export default async function Organization(){
         const organization = await getOrganization();
    
-    return <div className="min-h-screen flex justify-center items-center p-24">
+    return <div className="min-h-screen bg-gray-50">
+        <ToolBar></ToolBar>
             <DataTable columns={colunms} data={organization}
             ></DataTable>
+            
     </div>
 }
