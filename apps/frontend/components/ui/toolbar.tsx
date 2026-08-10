9dc2b6cb7d57fe6a0c59de0ef6cc1063216fb6b2
unknown <1606adarsh@gmail.com>
2026-08-10 20:36:38 +0530

    Toolbar

--- apps/frontend/components/ui/toolbar.tsx
+++ apps/frontend/components/ui/toolbar.tsx
@@ -1,3 +1,21 @@
-function ToolBar(){
-    return 
+import { Plus, User } from "lucide-react";
+import { AspectRatio, AspectRatioPortrait } from "./aspect-ratio";
+
+
+function ToolBar() {
+  return (
+    <nav className="bg-blue-500 w-full h-16 items-center flex justify-between border-b px-6">
+      <div className="gap-3 flex items-center">
+        <img src="/logo.png" alt="logo"  className="h-15 w-25 rounded-md" />
+        <span className="text-lg font-semibold">
+     StockFlow
+        </span>
+                <div className="flex items-center">
+                    <User size={22}> </User>
+                </div>
+      </div>
+    </nav>
+  );
 }
+
+export { ToolBar };
