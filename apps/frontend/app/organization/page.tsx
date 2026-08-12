554629e4336b602dc54d06c149f19689b86e52db
unknown <1606adarsh@gmail.com>
2026-08-12 20:47:57 +0530

    table add

--- apps/frontend/app/organization/page.tsx
+++ apps/frontend/app/organization/page.tsx
@@ -1,17 +1,35 @@
 import { colunms } from "@/app/column";
+import { InputInline } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { ToolBar } from "@/components/ui/toolbar";
 
 import { getOrganization } from "@/service/organization";
+import { Button } from "@base-ui/react";
 
+export default async function Organization() {
+  const organization = await getOrganization();
 
-export default async function Organization(){
-        const organization = await getOrganization();
-   
-    return <div className="min-h-screen bg-gray-50">
-        <ToolBar></ToolBar>
-            <DataTable columns={colunms} data={organization}
-            ></DataTable>
-            
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ToolBar></ToolBar>
+      <div className="w-full p-6">
+        <div className="mb-6 flex justify-between">
+          <div>
+            <h1 className="text-2xl font-bold tracking-normal">Organization</h1>
+            <p className="text-sm text-muted-foreground">
+              Manage your organization
+            </p>
+          </div>
+        
+        
+          <div className="mb-4">
+            <InputInline className="border-4 rounded-sm"></InputInline>
+          </div>
+        </div>
+      </div>
+      <div className="overflow-hidden shadow-sm rounded-xl bg-white border">
+      <DataTable columns={colunms} data={organization}></DataTable>
+      </div>
     </div>
+  );
 }
